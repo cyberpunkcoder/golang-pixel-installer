93141a504f6f1359ac51a5c5be8eb9ae23804f16
cyberpunkcoder <cyberpunkcoder@gmail.com>
2020-03-13 02:55:11 -0700

    Working uninstall function.

--- install.sh
+++ install.sh
@@ -1,19 +1,43 @@
 #! /bin/bash
 
 # Author: cyberpunkprogrammer (github.com/cyberpunkprogrammer)
-# Date: March 12, 2020
+# Date: March 13, 2020
+
+# Exit if a command in the script fails.
+set -o errexit
 
 USERHOME=$HOME
+GOPROGRAM=/usr/local/go
 GOHOME=$USERHOME/go
 PIXELHOME=$USERHOME/pixel
-
-
+ 
 function installGo {
 	echo "Installing go."
+
+	apt update
+
+	go version
+	go env
+
+	echo "Go installation complete."
+}
+
+function uninstallGo {
+	rm -rf $GOHOME
+	rm -rf $GOPROGRAM
+	sed -i '/export PATH=$PATH:\/usr\/local\/go\/bin/d' .profile
+	sed -i '/export GOPATH=$HOME\/go/d' .profile
+	
+	echo "Go uninstalled."
 }
 
 function installPixel {
 	echo "Installing pixel."
+}
+
+function uninstallPixel {
+	rm -rf $PIXELHOME
+	echo "Pixel uninstalled."
 }
 
 # Check for previous pixel installations.
@@ -22,28 +46,36 @@ then
 	read -r -p "Pixel installation found, would you like to remove it? [y/N] " response
         case "$response" in
                 [yY][eE][sS]|[yY])
-                        #sudo rm -rf #PIXELHOME
-                        echo "Pixel installation has been removed."
-                ;;
+                	uninstallPixel
+		;;
         esac
 fi
 
 # Check for previous go installations.
-if [ -d /usr/local/go ]
+if [ -d $GOPROGRAM ] || [ -d $GOHOME ]
 then
 	read -r -p "Go installation found, would you like to remove it? [y/N] " response
 	case "$response" in
 		[yY][eE][sS]|[yY])
-			#sudo rm -rf /usr/local/go
-			echo "Go installation has been removed."
-				
-			read -r -p "Would you like to install go? [y/N] " response
+			uninstallGo
+			
+			read -r -p "Would you like to reinstall go? [y/N] " response
 		 	case "$response" in
 				[yY][eE][sS]|[yY])
 			 		installGo
-			 	
 				;;
 		 	esac
 		;;
 	esac
+else
+	installGo
+
+	read -r -p "Would you like to install Pixel? [y/N] " response
+     		case "$response" in
+ 			[yY][eE][sS]|[yY])
+			installPixel
+		;;
+	esac
 fi
+
+
